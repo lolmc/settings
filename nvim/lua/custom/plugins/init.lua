a5436463ebe17c69bf237b81057aab39641c5045
Lol McBride <3874987+lolmc@users.noreply.github.com>
2024-11-06 11:06:56 +0000

    initial commit of Neovim config files

---
+++ nvim/lua/custom/plugins/init.lua
@@ -0,0 +1,23 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+{
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+  },
+  keys = {
+    {
+      "<leader>?",
+      function()
+        require("which-key").show({ global = false })
+      end,
+      desc = "Buffer Local Keymaps (which-key)",
+    },
+  },
+}
+return {}
